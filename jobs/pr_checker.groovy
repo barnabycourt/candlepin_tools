90d57ccbb51bced602bfedb3153dd2f82bce6f9c
Barnaby Court <bcourt@redhat.com>
2016-08-26 15:49:48 -0400

    Start building out the job

---
+++ jobs/pr_checker.groovy
@@ -0,0 +1,17 @@
+
+job('candlepin_pr_bugzilla_checker'){
+    wrappers {
+        // Enable access to the github & the bugzilla user
+        credentialsBinding {
+            string('github.token', 'github_api_token')
+            usernamePassword('bz.user', 'bz.password', 'bugzilla_token')
+        }
+    }
+
+    steps {
+        shell('echo "first step"')
+//        python {
+//           command ('print "APPLES!"')
+//        }
+    }
+}
