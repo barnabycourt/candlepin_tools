4685fbf5a51a7d36d6348559d26b38b9b76f28a6
Barnaby Court <bcourt@redhat.com>
2016-08-29 16:29:11 -0400

    a

--- jobs/pr_checker.py
+++ jobs/pr_checker.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python
 
+import os
+
+from bugzilla.rhbugzilla import RHBugzilla
+
 print 'Trying Something New'
+
+bugzilla_login = os.environ['bz.user']
+bugzilla_pwd = os.environ['bz.password']
+
+BUGZILLA_URL = 'https://bugzilla.redhat.com'
+BZ = RHBugzilla(url='%s/xmlrpc.cgi' % BUGZILLA_URL,
+                user=bugzilla_login, password=bugzilla_pwd)
+bz = BZ.getbug('1207721')
+
+print bz
